c0f78ba619ca7fa88721f71f2f5d1857f36c5241
Andrew Nelson <nelsand19@gmail.com>
2025-07-17 17:08:11 -0600

    work on plan for todos

--- src/todos.js
+++ src/todos.js
@@ -8,4 +8,18 @@ class Todo {
 }
 
 
+function newTodo(querrySelect)
+
 export default Todo
+
+//can we do forms for the list?
+//on button.click => form
+
+
+//<form action='helpMe.com/path' method='post'>
+    //<label for='name'>name:</label>
+    //input type='text' id='name
+//</form>
+
+
+//can forms be put on classes?
